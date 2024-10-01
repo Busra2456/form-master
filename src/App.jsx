ac49792eec88e58a85520cc34528980ce4aed080
Busra2456 <you@example.com>
2024-10-01 23:18:17 +0600

    ReusableForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
 
+import { Children } from 'react'
 import './App.css'
 import HookForm from './Components/HookForm/HookForm'
 import RefForm from './Components/RefForm/RefForm'
+import ReusableForm from './Components/ReusableForm/ReusableForm'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 import StatefulForm from './StatefulForm/StatefulForm'
 
 function App() {
+  const handleSignupSubmit = data =>{
+    console.log('sign up data',data)};
+
+    const handleUpdateSubmit = data =>{
+    console.log('update profile',data) }
 
 
   return (
     <>
-      
-      <h1>Simple Form</h1>
+     
+     <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
-    </>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm formTitle={'Sign Up'} handleSubmit={handleSignupSubmit}>
+
+        <div>
+          <h2>Sign Up</h2>
+          <p>please sign up right now</p>
+        </div>
+
+
+      </ReusableForm>
+    ;
+     <ReusableForm formTitle={'Profile Update'} submitBtnText='Update' handleSubmit={handleUpdateSubmit} >
+
+      <div>
+       <h2>Update Profile</h2>
+        <p>Always keep your profile updated</p>
+      </div>
+
+
+     </ReusableForm>
+     
+     </>
+
+
   )
 }
 
